1fc5170ad20b71198f283d57f5a725896c8d360a
Zikani Nyirenda Mwase <zikani.nmwase@ymail.com>
2018-10-03 11:52:53 +0200

    Remove "defaults" in the default Converters
    The default Converters had this concept of defaults when the
    parsing of the value from the cell failed - in this change
    we remove defaults in favour of returning nulls. Perhaps
    it would be better to throw an exception but for null this
    should suffice.
    Users should handle invalid values themselves

--- core/src/main/java/com/creditdatamw/zerocell/converter/Converters.java
+++ core/src/main/java/com/creditdatamw/zerocell/converter/Converters.java
@@ -11,16 +11,19 @@ import java.util.Objects;
 
 /**
  * Common Converters
+ *
+ * The converters will throw a ZeroCellException in case of failure to parse the
+ * provided.
  */
 public class Converters {
     private static final Logger LOGGER = LoggerFactory.getLogger(Converters.class);
 
     public static final Converter<Float> toFloat = ((formattedValue, columnName, row) -> {
         try {
-            return Float.valueOf(formattedValue == null ? "0.0" : formattedValue);
+            return Float.valueOf(formattedValue);
         } catch (Exception e) {
-            LOGGER.error("Failed to parse {} as float. Using default of -1 at column={} row={} ", formattedValue, columnName, row);
-            return new Float(-1.0);
+            LOGGER.error("Failed to parse {} as Float. Using default of null at column={} row={} ", formattedValue, columnName, row);
+            return null;
         }
     });
 
@@ -33,7 +36,7 @@ public class Converters {
             return LocalDateTime.parse(formattedValue);
         } catch(Exception e) {
             LOGGER.error("Failed to parse {} as Date. Using default of null at column={} row={} ", formattedValue, columnName, rowNum);
-            return LocalDateTime.of(1905, 01, 01, 0, 0, 0);
+            return null;
         }
     });
 
@@ -43,7 +46,7 @@ public class Converters {
             return LocalDate.parse(formattedValue);
         } catch(Exception e) {
             LOGGER.error("Failed to parse {} as Date. Using default of null at column={} row={} ", formattedValue, columnName, rowNum);
-            return LocalDate.of(1905, 01, 01);
+            return null;
         }
     });
 
@@ -56,10 +59,11 @@ public class Converters {
         }
     });
 
-    public static final Converter<Timestamp> toSqlTimestamp = ((formattedValue, columnName, row) -> {
+    public static final Converter<Timestamp> toSqlTimestamp = ((formattedValue, columnName, rowNum) -> {
         try {
-            return Timestamp.valueOf(formattedValue == null ? "1905-01-01" : formattedValue);
+            return Timestamp.valueOf(formattedValue);
         } catch (Exception e) {
+            LOGGER.error("Failed to parse {} as Timestamp. Using default of null at column={} row={} ", formattedValue, columnName, rowNum);
             return null;
         }
     });
@@ -68,28 +72,28 @@ public class Converters {
         try {
             return Integer.valueOf(formattedValue == null ? "0.0" : formattedValue);
         } catch (Exception e) {
-            LOGGER.error("Failed to parse {} as integer. Using default of -1 at column={} row={} ", formattedValue, columnName, rowNum);
-            return Integer.valueOf(-1);
+            LOGGER.error("Failed to parse {} as Integer. Using default of null at column={} row={} ", formattedValue, columnName, rowNum);
+            return null;
         }
     });
 
-    public static final Converter<Long> toLong = ((formattedValue, columnName, row) -> {
+    public static final Converter<Long> toLong = ((formattedValue, columnName, rowNum) -> {
         //} else if (fieldType == Long.class || fieldType == long.class) {
         try {
-            return Long.valueOf(formattedValue == null ? "0" : formattedValue);
+            return Long.valueOf(formattedValue);
         } catch (Exception e) {
-            // LOGGER.error("Failed to parse {} as long. Using default of -1 at column={} row={} ", formattedValue, columnName, rowNum);
-            return Long.valueOf(-1);
+            LOGGER.error("Failed to parse {} as Long. Using default of null at column={} row={} ", formattedValue, columnName, rowNum);
+            return null;
         }
     });
 
-    public static final Converter<Double> toDouble = ((formattedValue, columnName, row) -> {
+    public static final Converter<Double> toDouble = ((formattedValue, columnName, rowNum) -> {
         //  } else if (fieldType == Double.class || fieldType == double.class) {
         try {
-            return Double.valueOf(formattedValue == null ? "0.0" : formattedValue);
+            return Double.valueOf(formattedValue);
         } catch (Exception e) {
-            // LOGGER.error("Failed to parse {} as double. Using default of -1 at column={} row={} ", formattedValue, columnName, rowNum);
-            return Double.valueOf(-1.0);
+            LOGGER.error("Failed to parse {} as Double. Using default of null at column={} row={} ", formattedValue, columnName, rowNum);
+            return null;
         }
     });
 
@@ -97,8 +101,8 @@ public class Converters {
         try {
             return Boolean.valueOf(formattedValue == null ? "FALSE" : "TRUE");
         } catch(Exception e) {
-            //LOGGER.error("Failed to parse {} as Boolean. Using default of null at column={} row={} ", formattedValue, columnName, rowNum);
-            return Boolean.FALSE;
+            LOGGER.error("Failed to parse {} as Boolean. Using default of null at column={} row={} ", formattedValue, columnName, rowNum);
+            return null;
         }
     });
 
