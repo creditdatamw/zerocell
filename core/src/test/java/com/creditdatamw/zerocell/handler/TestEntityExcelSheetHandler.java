3acb1bbdfee72a17eb5a224eadd6d9d1046fc9b4
Zikani Nyirenda Mwase <zikani@creditdatamw.com>
2019-08-15 00:05:55 +0200

    Add tests for converter and handlers

---
+++ core/src/test/java/com/creditdatamw/zerocell/handler/TestEntityExcelSheetHandler.java
@@ -0,0 +1,30 @@
+package com.creditdatamw.zerocell.handler;
+
+import com.creditdatamw.zerocell.Person;
+import com.creditdatamw.zerocell.column.ColumnInfo;
+import com.creditdatamw.zerocell.column.ColumnMapping;
+import org.junit.Test;
+
+/**
+ * Tests for the EntityExcelSheetHandler
+ */
+public class TestEntityExcelSheetHandler {
+
+    @Test
+    public void testHandlerWithDefaults() {
+        EntityHandler<Person> entityHandler = new EntityHandler<>(
+            Person.class,
+            true,
+            0
+        );
+
+        ColumnMapping mapping = ColumnMapping.parseColumnMappingFromAnnotations(Person.class);
+        EntityExcelSheetHandler<Person> sheetHandler =
+            new EntityExcelSheetHandler<Person>(
+                entityHandler,
+                mapping.getRowNumberInfo(),
+                null // mapping.getColumns().toArray()
+            );
+
+    }
+}
---
+++ core/src/test/java/com/creditdatamw/zerocell/handler/TestEntityHandler.java
@@ -0,0 +1,24 @@
+package com.creditdatamw.zerocell.handler;
+
+import com.creditdatamw.zerocell.Person;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class TestEntityHandler {
+
+    @Test
+    public void testHandlerWithDefaults() {
+        EntityHandler<Person> entityHandler = new EntityHandler<>(
+            Person.class,
+            true,
+            0
+        );
+
+        assertEquals(Person.class, entityHandler.getEntityClass());
+        assertEquals("uploads", entityHandler.getSheetName());
+        assertTrue(entityHandler.isSkipHeaderRow());
+        assertEquals(0, entityHandler.getSkipFirstNRows());
+    }
+}
