90e09f7162f893c66f10d51b52c633dba2b9e945
Zikani Nyirenda Mwase <zikani@creditdatamw.com>
2019-08-16 22:41:33 +0200

    Implement tests for ColumnInfo

---
+++ core/src/test/java/com/creditdatamw/zerocell/column/TestRowNumberInfo.java
@@ -0,0 +1,16 @@
+package com.creditdatamw.zerocell.column;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestRowNumberInfo {
+    @Test
+    public void testConstructor() {
+        RowNumberInfo rowNumberInfo = new RowNumberInfo("rowNo", Integer.class);
+
+        assertEquals("rowNo", rowNumberInfo.getFieldName());
+        assertEquals(Integer.class, rowNumberInfo.getType());
+        assertEquals("__rowNUmber__", rowNumberInfo.getName());
+    }
+}
